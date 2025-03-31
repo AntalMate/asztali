d707e99a54c3af50ad9631e87e236c7b708c09ca
AntalMate <antalm@kkszki.hu>
2025-03-31 08:04:39 +0200

    romanroman

--- mysqllogin/mysqllogin/MainWindow.xaml.cs
+++ mysqllogin/mysqllogin/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace mysqllogin
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
