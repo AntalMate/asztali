b8231b986d7fbdc2ee187936b875eb20224d6f5a
antalmd <antalm@kkszki.hu>
2025-04-28 09:25:03 +0200

    asdasd

--- antalm_termek/antalm_termek/MainWindow.xaml.cs
+++ antalm_termek/antalm_termek/MainWindow.xaml.cs
@@ -21,31 +21,51 @@ namespace antalm_termek
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int selId;
         MySqlConnection kapcs = new MySqlConnection("server = localhost;database = antalm_termek; uid = root; password = ''");
         public MainWindow()
         {
             InitializeComponent();
-            InitializeComponent();
-            kapcs.Open();
-            var sql = "SELECT * FROM antalm_termek;";
-            var parancs = new MySqlCommand(sql, kapcs);
-            var lekerdezes = parancs.ExecuteReader();
-            while (lekerdezes.Read())
-            {
-                lbTermekek.Items.Add(lekerdezes["id"] + " " + lekerdezes["cikkszam"] + " " + lekerdezes["megnevezes"]);
-            }
-            lekerdezes.Close();
-
-            kapcs.Close();
+            frissit();
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             kapcs.Open();
             var sql = $"INSERT INTO antalm_termek (cikkszam, megnevezes) VALUES ('{txtCikkszam.Text}', '{txtMegnevezes.Text}');";
             new MySqlCommand(sql, kapcs).ExecuteNonQuery();
-            lbTermekek.Items.Clear();
-            var parancs = new MySqlCommand("SELECT * FROM antalm_termek;", kapcs);
+            kapcs.Close();
+            frissit();
+        }
+
+        private void lbTermekek_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Az éppen kiválasztott elem cikkszámát és megnevezését kiírja a szövegdobozokba
+            var selectedItem = lbTermekek.SelectedItem.ToString();
+            var parts = selectedItem.Split(' ');
+            modCikk.Text = parts[1];
+            modNev.Text = parts[2];
+            selId = Convert.ToInt32(parts[0]);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            kapcs.Open();
+            var sql = $"UPDATE antalm_termek SET cikkszam = '{modCikk.Text}' WHERE id= '{selId}';";
+            var sql2 = $"UPDATE antalm_termek SET megnevezes = '{modNev.Text}' WHERE id= '{selId}';";
+            new MySqlCommand(sql, kapcs).ExecuteNonQuery();
+            new MySqlCommand(sql2, kapcs).ExecuteNonQuery();
+            MessageBox.Show("Sikeres módosítás!");
+            kapcs.Close();
+            frissit();
+        }
+
+        private void frissit()
+        {
+            kapcs.Open();
+            var sql = "SELECT * FROM antalm_termek;";
+            var parancs = new MySqlCommand(sql, kapcs);
             var lekerdezes = parancs.ExecuteReader();
+            lbTermekek.Items.Clear();
             while (lekerdezes.Read())
             {
                 lbTermekek.Items.Add(lekerdezes["id"] + " " + lekerdezes["cikkszam"] + " " + lekerdezes["megnevezes"]);
